896eb431e6d285729542ceac14475508d4515246
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-07-20 12:10:54 -0300

    Update test subscribeButton and Tests Page Home

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,15 +1,18 @@
-import { render,screen} from '@testing-library/react'
+import { render,screen, fireEvent} from '@testing-library/react'
 import {SubscribeButton} from '.'
-import {useSession} from 'next-auth/client'
+import {useSession, signIn} from 'next-auth/client'
 import {mocked} from 'jest-mock'
+import  {useRouter}  from 'next/router'
 
 jest.mock('next-auth/client')
+jest.mock('next/router')
 
-
-describe('SignInButton component', ()=>{ 
+describe('SubscribeButton component', ()=>{ 
     
     it('renders correctly ', () =>{
-        
+        const useSessionMocked =mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce([null,false])
 
         render(<SubscribeButton/>)
         
@@ -17,4 +20,48 @@ describe('SignInButton component', ()=>{
  
     })
 
+    it('redirects user to sign in when not authenticated' , ()=>{
+        const signInMocked = mocked(signIn)
+
+        const useSessionMocked =mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce([null,false])
+
+        render(<SubscribeButton/>)
+
+        const subscribeButton = screen.getByText('Subscribe now');
+        
+        fireEvent.click(subscribeButton)
+
+        expect(signInMocked).toHaveBeenCalled()
+    })
+    it('redirects tp posts when user already has a subiscription', ()=>{
+        const useRouterMocked = mocked(useRouter)
+        const useSessionMocked = mocked(useSession)
+        const pushMock = jest.fn()
+
+        useSessionMocked.mockReturnValueOnce([    
+            {
+                user: { 
+                  name: "John Doe", 
+                  email: "john.doe@example.com" 
+                },
+                activeSubscription: 'fake-active-subscription',
+                expires: "fake-expires",
+              },
+              false
+        ] as any)
+
+        useRouterMocked.mockReturnValueOnce({
+            push: pushMock,
+        } as any)   // as any para arrumar problemas com tipagem
+
+        render(<SubscribeButton/>)
+
+        const subscribeButton = screen.getByText('Subscribe now')
+
+        fireEvent.click(subscribeButton)
+
+        expect(pushMock).toHaveBeenCalledWith('/posts')
+    })
 })
