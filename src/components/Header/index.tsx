0e41ff379477d06e9077f5a9574cac1a248c97cf
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-08-17 12:42:21 -0300

    update index posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ export function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt=""/>
+                <img src="/images/logo.svg" alt="logotipo"/>
                 <nav>
                     <ActiveLink activeClassName ={styles.active} href="/">
                         <a>Home</a>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -45,7 +45,7 @@ export const getStaticProps: GetStaticProps = async () => {
     const response = await prismic.query([
         Prismic.predicates.at('document.type', 'publication')
     ], {
-        fetch: ['publication.title', 'publication.content'],
+        fetch: ['title', 'content'],
         pageSize: 100,
     })
     
@@ -66,6 +66,8 @@ export const getStaticProps: GetStaticProps = async () => {
     })
 
     return {
-        props: { posts }
+        props: { 
+            posts 
+        }
     }
 }
