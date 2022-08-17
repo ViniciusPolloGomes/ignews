88481169e585cbc2a5efcd3a87f0495df74b322d
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-08-17 16:22:49 -0300

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,7 +5,7 @@ declare module "next-auth" {
 
 Returned by useSession, getSession and received as a prop on the Provider React Context
 */
-interface Session {
-activeSubscription: object | boolean;
-}
+    interface Session {
+        activeSubscription: object | boolean;
+    }
 }
