fbcd34b072f9feacfb6c53b6240819deccdbcc86
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-08-17 13:09:16 -0300

    update page / index

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -25,7 +25,7 @@ export default function Posts( {posts} : PostsProps ) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                        <Link key={post.slug} href={`/posts/${post.slug}`}>
+                        <Link key={post.slug} href={`https://ignews-dusky.vercel.app/posts/preview/${post.slug}`}>
                             <a>
                                 <time>{post.updatedAt}</time>
                                 <strong>{post.title}</strong>
